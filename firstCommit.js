ab424f148552c3299b6933b1d0c8aae222a9d516
Mac Evans <maclean.evans@gmail.com>
2015-08-20 18:46:30 -0500

    Working on getting four options

---
+++ firstCommit.js
@@ -0,0 +1,27 @@
+var answer1 = "Archie Griffin",
+    answer2 = "Germany",
+    answer3 = "Four";
+
+var userAnswer1 = prompt("Who is the only person that has won the Heisman twice?")
+if (userAnswer1 == answer1) {
+
+    alert("Correct")
+} else {
+    alert("Sorry, that was wrong")
+}
+var userAnswer2 = prompt("Who won the last FIFA World Cup?")
+
+if (userAnswer2 == answer2) {
+    alert("Correct")
+} else {
+    alert("Sorry, that was wrong")
+}
+
+var userAnswer3 = prompt("How many faces are on Mount Rushmore?")
+
+if (userAnswer3 == answer3) {
+    alert("Correct")
+} else {
+    alert("Sorry, that was wrong")
+}
+// All of this works as a general quiz but does not follow the four options guideline
